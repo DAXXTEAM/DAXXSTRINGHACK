e52b06619251534b1a56b991d619ea104f70fb5f
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-10-08 00:41:24 +0530

    Create __main__.py

---
+++ DAXXSTRINGHACK/__main__.py
@@ -0,0 +1,19 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+from DAXXSTRINGHACK import LOG
+from DAXXSTRINGHACK.modules import ALL_MODULES
+
+
+async def start_bot():
+    for all_module in ALL_MODULES:
+        importlib.import_module("DAXXSTRINGHACK.modules." + all_module)
+    LOG.print("[bold yellow]𝗛𝗔𝗖𝗞 𝐁𝐎𝐓 𝐒𝐓𝐀𝐑𝐓𝐄𝐃 𝐍𝐎𝐖 𝗙𝗨𝗖𝗞 𝗔𝗟𝗟 𝗧𝗚 𝗜𝗗")
+    await idle() 
+    LOG.print("[bold red]𝐂𝐀𝐍𝐂𝐋𝐄 𝐀𝐋𝐋 𝐓𝐀𝐒𝐊🤐..........")
+
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(start_bot())
