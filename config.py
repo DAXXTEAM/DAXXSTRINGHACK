60cea0fb4fb2731195af08d097c6071a6ea38864
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-10-14 14:44:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # TEAM DAXX ALL COPYRIGHT ©️
 class Config:
-    API_ID = ""
-    API_HASH = ""
-    TOKEN = ""
-    START_PIC = ""
+    API_ID = "20136640"
+    API_HASH = "32fa482ffd34ceefec26b7ccd73c4fbb"
+    TOKEN = "6437116754:AAFlq2FZ3E-KLM4aOIFT7iothxMjygwa9n4"
+    START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
