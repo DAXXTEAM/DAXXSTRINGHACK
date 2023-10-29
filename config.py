3fec1c50b99b109186d7f337cb8357ac1de98e79
ƛɗɩtyɑ Ɦɑɭɗɘr <144584943+AdityaHalder@users.noreply.github.com>
2023-10-29 19:11:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 # TEAM DAXX ALL COPYRIGHT ©️
+from pyrogram import filters
+
 class Config:
     API_ID = "20136640"
     API_HASH = "32fa482ffd34ceefec26b7ccd73c4fbb"
     TOKEN = "6437116754:AAFlq2FZ3E-KLM4aOIFT7iothxMjygwa9n4"
     MONGO_URL = "mongodb://mongo:4cGfdCA3cHc3F5ec2Cg6b3eDC-E21AGb@viaduct.proxy.rlwy.net:49022"
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
+    SUDOERS = filters.user("12345678")
