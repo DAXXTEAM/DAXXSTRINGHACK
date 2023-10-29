7bb2eb946b1e075cf3d20b80dda8b8c17793b9f2
ƛɗɩtyɑ Ɦɑɭɗɘr <144584943+AdityaHalder@users.noreply.github.com>
2023-10-29 18:43:12 +0530

    Create mongo.py

---
+++ DAXXSTRINGHACK/Helpers/mongo.py
@@ -0,0 +1,61 @@
+import sys
+
+from config import Config
+from motor.motor_asyncio import AsyncIOMotorClient
+
+# Mongo Database
+try:
+    mongodb = AsyncIOMotorClient(Config.MONGO_URL).Daxx
+except:
+    print("Please Change Your MongoDB")
+    sys.exit()
+
+
+chatsdb = mongodb.tgchatsdb
+usersdb = mongodb.tgusersdb
+
+
+# All Served Users
+async def is_served_user(user_id: int) -> bool:
+    user = await usersdb.find_one({"user_id": user_id})
+    if not user:
+        return False
+    return True
+
+
+async def get_served_users() -> list:
+    users_list = []
+    async for user in usersdb.find({"user_id": {"$gt": 0}}):
+        users_list.append(user)
+    return users_list
+
+
+async def add_served_user(user_id: int):
+    is_served = await is_served_user(user_id)
+    if is_served:
+        return
+    return await usersdb.insert_one({"user_id": user_id})
+
+
+# All Served Chats
+async def get_served_chats() -> list:
+    chats_list = []
+    async for chat in chatsdb.find({"chat_id": {"$lt": 0}}):
+        chats_list.append(chat)
+    return chats_list
+
+
+async def is_served_chat(chat_id: int) -> bool:
+    chat = await chatsdb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
+
+
+async def add_served_chat(chat_id: int):
+    is_served = await is_served_chat(chat_id)
+    if is_served:
+        return
+    return await chatsdb.insert_one({"chat_id": chat_id})
+
+
