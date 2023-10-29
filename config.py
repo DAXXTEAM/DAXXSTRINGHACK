5399c09a8a32ffbe12a315ca9abfce668153cddb
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-10-29 19:44:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 class Config:
     API_ID = "20136640"
     API_HASH = "32fa482ffd34ceefec26b7ccd73c4fbb"
-    TOKEN = "6437116754:AAFlq2FZ3E-KLM4aOIFT7iothxMjygwa9n4"
-    MONGO_URL = "mongodb://mongo:4cGfdCA3cHc3F5ec2Cg6b3eDC-E21AGb@viaduct.proxy.rlwy.net:49022"
+    TOKEN = "6521122303:AAGymB4jol2qMgXHXKswMM5N7fu5zt_WkHc"
+    MONGO_URL = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
-    SUDOERS = filters.user(["12345678"])
+    SUDOERS = filters.user(["6691393517"])
