7674c6b98a079f5abcf74b1188cb180e4f4791dd
ƛɗɩtyɑ Ɦɑɭɗɘr <144584943+AdityaHalder@users.noreply.github.com>
2023-10-29 19:11:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ class Config:
     TOKEN = "6437116754:AAFlq2FZ3E-KLM4aOIFT7iothxMjygwa9n4"
     MONGO_URL = "mongodb://mongo:4cGfdCA3cHc3F5ec2Cg6b3eDC-E21AGb@viaduct.proxy.rlwy.net:49022"
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
-    SUDOERS = filters.user("12345678")
+    SUDOERS = filters.user(["12345678"])
