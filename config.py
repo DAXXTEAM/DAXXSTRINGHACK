2f80334eb927bea9b2bcc9c73ca6af2c97a78778
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-11-12 22:25:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 class Config:
     API_ID = "20136640"
     API_HASH = "32fa482ffd34ceefec26b7ccd73c4fbb"
-    TOKEN = "6521122303:AAGymB4jol2qMgXHXKswMM5N7fu5zt_WkHc"
+    TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     MONGO_URL = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
     SUDOERS = filters.user(["6691393517"])
